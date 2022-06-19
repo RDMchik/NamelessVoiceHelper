419d5a6daf1d188dfc835cf52e9ec5445095368c
RDMchik <denkickgames@gmail.com>
2022-06-19 13:50:15 +0300

    'initial'

--- lib/utils/temp_clean.py
+++ lib/utils/temp_clean.py
@@ -5,8 +5,12 @@ class TempCleaner(object):
     """cleans temp sub directories"""
 
     TEMP_AUDIO_DIRECTORY = 'temp\\audio'
+    TEMP_IMAGES_DIRECTORY = 'temp\\images'
 
     def clear() -> None:
 
         for file in os.listdir(TempCleaner.TEMP_AUDIO_DIRECTORY):
             os.remove(os.path.join(TempCleaner.TEMP_AUDIO_DIRECTORY, file))
+
+        for file in os.listdir(TempCleaner.TEMP_IMAGES_DIRECTORY):
+            os.remove(os.path.join(TempCleaner.TEMP_IMAGES_DIRECTORY, file))
